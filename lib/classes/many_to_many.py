6c13f2642fde690b5e74280bf98c29fb49c45fd1
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-12-08 20:11:05 +0300

    changes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,87 @@
 class Article:
+    all_articles = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
         
+        Article.all_articles.append(self)
+
+    @property  
+    def title(self):
+        return self._title
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in Article.all_articles if article.author == self})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        topic_areas = {magazine.category for magazine in self.magazines()}
+        return list(topic_areas) if topic_areas else None
+
 
 class Magazine:
     def __init__(self, name, category):
+        self._name = None
+        self._category = None
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Magazine name must be a string.")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Category must be a string.")
+        if not value:
+            raise ValueError("Category cannot be empty.")
+        self._category = value
+
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self.articles():
+            author_counts[article.author] = author_counts.get(article.author, 0) + 1
+        return [author for author, count in author_counts.items() if count > 2] or None
+    
+# # INSTANCES FOR ALL CLASSES AND ASSOCIATIONS
+
+
+
+
+
